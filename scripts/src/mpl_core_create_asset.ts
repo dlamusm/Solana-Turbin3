deb6821946755946dd99c0429b72837bf894aa35
Diego Lamus <dlamusm@gmail.com>
2024-09-09 17:44:25 +0100

    Update scripts

--- scripts/src/mpl_core_create_asset.ts
+++ scripts/src/mpl_core_create_asset.ts
@@ -1,6 +1,6 @@
 import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
 import { createSignerFromKeypair, signerIdentity, createGenericFile, createGenericFileFromJson, generateSigner} from '@metaplex-foundation/umi'
-import { createCollection } from '@metaplex-foundation/mpl-core'
+import { create } from '@metaplex-foundation/mpl-core'
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
 import { readFile } from "fs/promises"
 import wallet from '../wallet.json'
@@ -9,10 +9,10 @@ import path from 'path'
 // config
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
 const IMAGE_FILE = `${path.dirname(__dirname)}/images/dog-wif-crown-1.jpg`;
-const TARGET_IMAGE_FILE_NAME = "dog-wif-crown-logo.jpg";
-const COLLECTION_NAME = "Dog wif crown";
-const COLLECTION_SYMBOL = "DWC";
-const COLLECTION_DESCRIPTION = "Dogs with crown NFT collection";
+const TARGET_IMAGE_FILE_NAME = "dog-wif-crown-1.jpg";
+const ASSET_NAME = "Dog wif crown 1";
+const ASSET_DESCRIPTION = "Dogs with crown NFT asset";
+const ASSET_COLLECTION = ""  // modify pubkey
 
 // Create a UMI connection
 const umi = createUmi(RPC_ENDPOINT);
@@ -22,7 +22,7 @@ umi.use(signerIdentity(signer));
 umi.use(irysUploader());
 
 (async () => {
-    // Upload collection image 
+    // Upload asset image 
     const imageFileBuiffer = await readFile(IMAGE_FILE)
     const imageGenericFile = createGenericFile(
         imageFileBuiffer,
@@ -34,9 +34,8 @@ umi.use(irysUploader());
 
     // Create off chain file
     const metadataJson = {
-        name: COLLECTION_NAME,
-        symbol: COLLECTION_SYMBOL,
-        description: COLLECTION_DESCRIPTION,
+        name: ASSET_NAME,
+        description: ASSET_DESCRIPTION,
         image: imageUri[0],
         properties: {
             files: [
@@ -51,19 +50,19 @@ umi.use(irysUploader());
     const offchainDataJsonGenericFile = createGenericFileFromJson(metadataJson)
     const offchainDataUri = await umi.uploader.upload([offchainDataJsonGenericFile]);
 
-    console.log(`Collection image URI: ${imageUri[0]}`);
-    console.log(`Collection offchain data URI: ${offchainDataUri[0]}`)
+    console.log(`Asset image URI: ${imageUri[0]}`);
+    console.log(`Asset offchain data URI: ${offchainDataUri[0]}`)
 
-    // Create collection
-    const collectionSigner = generateSigner(umi)
-    const collectionArgs = {
-        collection: collectionSigner,
-        name: COLLECTION_NAME,
+    // Create Asset
+    const assetSigner = generateSigner(umi)
+    const assetArgs = {
+        asset: assetSigner,
+        name: ASSET_NAME,
         uri: offchainDataUri[0],
     };
 
-    const tx = await createCollection(umi, collectionArgs).sendAndConfirm(umi);
+    const tx = await create(umi, assetArgs).sendAndConfirm(umi);
 
-    console.log(`Collection pubkey: ${collectionSigner.publicKey}`);
+    console.log(`Asset pubkey: ${assetSigner.publicKey}`);
 
 })();
