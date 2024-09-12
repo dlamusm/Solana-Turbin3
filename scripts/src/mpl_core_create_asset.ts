a85cc3411f507b98b9bde64d826ba25357ad78f8
Diego Lamus <dlamusm@gmail.com>
2024-09-12 18:43:57 +0100

    Update scripts

--- scripts/src/mpl_core_create_asset.ts
+++ scripts/src/mpl_core_create_asset.ts
@@ -1,10 +1,11 @@
 import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
 import { createSignerFromKeypair, signerIdentity, createGenericFile, createGenericFileFromJson, generateSigner} from '@metaplex-foundation/umi'
-import { create } from '@metaplex-foundation/mpl-core'
+import { create, fetchAsset, fetchCollection } from '@metaplex-foundation/mpl-core'
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
 import { readFile } from "fs/promises"
 import wallet from '../wallet.json'
 import path from 'path'
+import bs58 from 'bs58'
 
 // config
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
@@ -12,7 +13,7 @@ const IMAGE_FILE = `${path.dirname(__dirname)}/images/dog-wif-crown-1.jpg`;
 const TARGET_IMAGE_FILE_NAME = "dog-wif-crown-1.jpg";
 const ASSET_NAME = "Dog wif crown 1";
 const ASSET_DESCRIPTION = "Dogs with crown NFT asset";
-const ASSET_COLLECTION = ""  // modify pubkey
+const ASSET_COLLECTION = "EZ4QiPHTycnRTN73T2cyNu5LKwmhittq64gPdayoMVVn"
 
 // Create a UMI connection
 const umi = createUmi(RPC_ENDPOINT);
@@ -52,17 +53,32 @@ umi.use(irysUploader());
 
     console.log(`Asset image URI: ${imageUri[0]}`);
     console.log(`Asset offchain data URI: ${offchainDataUri[0]}`)
+   
+    // fetch collection
+    const collection = await fetchCollection(umi, ASSET_COLLECTION)
+    console.log('Collection:')
+    console.log(collection)
 
     // Create Asset
     const assetSigner = generateSigner(umi)
     const assetArgs = {
+        collection: collection,
         asset: assetSigner,
         name: ASSET_NAME,
         uri: offchainDataUri[0],
     };
 
     const tx = await create(umi, assetArgs).sendAndConfirm(umi);
+    console.log(`Transaction signature: ${bs58.encode(Buffer.from(tx.signature))}`)
 
-    console.log(`Asset pubkey: ${assetSigner.publicKey}`);
+    // fetch asset
+    const asset = await fetchAsset(umi, assetSigner.publicKey)
+    console.log('Created Asset:')
+    console.log(asset)
+
+    // fetch collection again
+    const updatedCollection = await fetchCollection(umi, ASSET_COLLECTION)
+    console.log('Updated Collection:')
+    console.log(updatedCollection)
 
 })();
--- scripts/src/mpl_core_create_collection.ts
+++ scripts/src/mpl_core_create_collection.ts
@@ -1,14 +1,15 @@
 import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
 import { createSignerFromKeypair, signerIdentity, createGenericFile, createGenericFileFromJson, generateSigner} from '@metaplex-foundation/umi'
-import { createCollection } from '@metaplex-foundation/mpl-core'
+import { createCollection, fetchCollection } from '@metaplex-foundation/mpl-core'
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
 import { readFile } from "fs/promises"
 import wallet from '../wallet.json'
 import path from 'path'
+import bs58 from 'bs58'
 
 // config
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
-const IMAGE_FILE = `${path.dirname(__dirname)}/images/dog-wif-crown-1.jpg`;
+const IMAGE_FILE = `${path.dirname(__dirname)}/images/dog-wif-crown-logo.jpg`;
 const TARGET_IMAGE_FILE_NAME = "dog-wif-crown-logo.jpg";
 const COLLECTION_NAME = "Dog wif crown";
 const COLLECTION_SYMBOL = "DWC";
@@ -63,7 +64,11 @@ umi.use(irysUploader());
     };
 
     const tx = await createCollection(umi, collectionArgs).sendAndConfirm(umi);
+    console.log(`Transaction signature: ${bs58.encode(Buffer.from(tx.signature))}`)
 
-    console.log(`Collection pubkey: ${collectionSigner.publicKey}`);
+    // fetch collection
+    const collection = await fetchCollection(umi, collectionSigner.publicKey)
+    console.log('Collection:')
+    console.log(collection)
 
 })();
